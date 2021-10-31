912a5d8175672e6ecb2bc2235aca473424a536e2
Aayush Gupta <aayushgupta219@gmail.com>
2021-10-31 05:05:25 +0000

    DNM: sdm660-common: Comment out ANT+ targets
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -85,12 +85,12 @@ PRODUCT_PACKAGES += \
     fs_config_files
 
 # ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
+#PRODUCT_PACKAGES += \
+#    AntHalService \
+#    com.dsi.ant.antradio_library
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+#PRODUCT_COPY_FILES += \
+#    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
