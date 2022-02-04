91a8bd9a9eb3e552746103cb7984e239aec892ed
Sal Savage <salsavage@google.com>
2022-02-04 09:41:57 -0800

    sdm660-common: Enable bluetooth profiles with product.prop
    Bug: 217448211
    Test: make -j
    Change-Id: Idd7e52b7822915ed61299f1c0b29bf87ca0339fe

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,6 +103,7 @@ TARGET_KERNEL_CLANG_VERSION := r416183b1
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Partitions
