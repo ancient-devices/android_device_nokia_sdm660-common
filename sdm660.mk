425b8c0a213670b52515e15bbc865dadd9b8e69c
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    sdm660-common: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- sdm660.mk
+++ sdm660.mk
@@ -161,10 +161,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_megvii
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Display
 PRODUCT_PACKAGES += \
 	android.frameworks.displayservice@1.0.vendor \
